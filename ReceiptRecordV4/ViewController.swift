35131dfb9f73816605d2d53a401045f3926f0a92
chenyuhang01@gmail.com <chenyuhang01@gmail.com>
2022-03-27 22:05:31 +0800

    Initial Commit

---
+++ ReceiptRecordV4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReceiptRecordV4
+//
+//  Created by Chen Yu Hang on 27/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
